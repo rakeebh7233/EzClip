b23a7ac1671a4e627c5f9bfb8212e4afaa4a789a
Rakeeb Hossain <hossainrakeeb7233@gmail.com>
2025-11-10 15:06:46 -0500

    Project Setup completed. Prestyled CSS and assets added. Navbar added.

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <main className='wrapper page'>
+
+      <h1 className='text-2xl font-karla'>Welcome to Loom Clone</h1>
+      <div>Page</div>
+    </main>
+  )
+}
+
+export default Page
---
+++ components/Navbar.tsx
@@ -0,0 +1,32 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+const user = {};
+
+const Navbar = () => {
+  return (
+    <header className='navbar'>
+      <nav>
+        <Link href="/">
+          <Image src="/assets/icons/logo.svg" alt='Logo' width={32} height={32} />
+          <h1>EzClip</h1>
+        </Link>
+
+
+        {user && (
+          <figure>
+            <button>
+              <Image src="/assets/images/dummy.jpg" alt='User' width={36} height={36} className='rounded-full aspect-square' />
+            </button>
+            <button className='cursor-pointer'>
+              <Image src="/assets/icons/logout.svg" alt='logout' width={24} height={24} className='rotate-180' />
+            </button>
+          </figure>
+        )}
+      </nav>
+
+    </header>
+  )
+}
+
+export default Navbar
