a8912ec2ebde025a5f2aa13361ef83f50cf973f8
Rakeeb Hossain <hossainrakeeb7233@gmail.com>
2025-11-14 01:06:24 -0500

    Header Component completed besides drop down

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,12 @@
+import Header from '@/components/Header'
 import React from 'react'
 
 const Page = () => {
   return (
     <main className='wrapper page'>
+      <Header title="All Videos" subHeader='Public Library'/>
 
       <h1 className='text-2xl font-karla'>Welcome to Loom Clone</h1>
-      <div>Page</div>
     </main>
   )
 }
